87d331dcc0fbdf626333fab5645d96742a18d197
Elad Hakmon <eladhakmon11@gmail.com>
2022-06-22 17:56:23 +0300

    Update my Work Experience

--- src/app/components/education-experience/education-experience.component.ts
+++ src/app/components/education-experience/education-experience.component.ts
@@ -3,11 +3,9 @@
 
   
   const ELEMENT_DATA: PeriodicElement[] = [
-    {position: "*", year: '2020 - currently', explanation: 'Barel Alon. Media, Digital and Purchasing Manager.'},
-    {position: "*", year: '2018 - currently', explanation: 'Business Events. Production Companies.'},
-    {position: "*", year: '2019', explanation: 'Graduate of the Full Stack Course at John Bryce College. GPA 90+'},
+    {position: "*", year: '2020', explanation: 'Graduate Full Stack Course at John Bryce College. GPA 90+ (600 hours)'},
+    {position: "*", year: '2018',  explanation: 'As part of my military service I was Professional Athlete.'},
     {position: "*", year: '2018',  explanation: 'Bartenders. Whiskey Bar & Museum.'},
-    {position: "*", year: '2018',  explanation: 'Car mechanic as part of my military service.'},
     {position: "*", year: '2018',  explanation: 'Graduate High School. Completion 12 years of schooling. Excellence in social activity.'},
     {position: "*", year:'2016' ,  explanation: 'Customer Service. sales center.'},
    ];
